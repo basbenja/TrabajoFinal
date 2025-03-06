282393ac6bf15879787f382eef5449bcde6bc0bd
Benjamín Bas <bbas@ascentio.com.ar>
2025-03-06 18:05:26 -0300

    cambios menores

--- Experimentos/utils/train_predict.py
+++ Experimentos/utils/train_predict.py
@@ -11,7 +11,6 @@ def train_step(model, dataloader, loss_fn, optimizer):
     """
     num_batches = len(dataloader)
     total_loss = 0
-    
     model.train()
     model_device = next(model.parameters()).device
     for batch, (X, y) in enumerate(dataloader):
@@ -24,7 +23,7 @@ def train_step(model, dataloader, loss_fn, optimizer):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        
+
         # total_loss += loss.item()
 
     # avg_loss = total_loss / num_batches
@@ -61,26 +60,26 @@ def validate_step(model, dataloader, loss_fn):
     num_batches = len(dataloader)
     size = len(dataloader.dataset)
     total_loss, total_accuracy = 0, 0
-    
+
     model.eval()
     model_device = next(model.parameters()).device
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(model_device), y.to(model_device)
             y_pred = model(X)
-            
+
             if isinstance(loss_fn, nn.BCEWithLogitsLoss):
                 loss = loss_fn(y_pred.view(-1), y)
             else:
                 loss = loss_fn(y_pred, y)
             total_loss += loss.item()
-            
+
             y_pred = predict(y_pred, loss_fn).squeeze()
             total_accuracy += (y_pred == y).type(torch.float).sum().item()
-    
+
     avg_loss = total_loss / num_batches
     avg_acc = total_accuracy / size
-    
+
     return avg_acc, avg_loss
 
 
@@ -89,14 +88,14 @@ def train_validate_loop(
 ):
     y_train_accs, train_losses = [], []
     y_test_accs , test_losses  = [], []
-    
+
     for epoch in tqdm(range(1, num_epochs+1)):
         tqdm.write(f"Epoch {epoch}")
         train_step(model, train_dataloader, loss_fn, optimizer)
-        
+
         train_acc, train_loss = validate_step(model, train_dataloader, loss_fn)
         test_acc , test_loss  = validate_step(model, test_dataloader , loss_fn)
-        
+
         tqdm.write(f"Train loss: {train_loss:.4f} | Train accuracy: {train_acc:.4f}")
         tqdm.write(f"Test loss : {test_loss:.4f} | Test accuracy : {test_acc:.4f}")
         tqdm.write("----------------------------------------------------------------")
